6aad3bcab0c330e97444dc45785ca0fccec83084
Anubhav Mishra <manubhav731@gmail.com>
2024-12-25 03:55:50 +0530

    APIs Created

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ const app = express();
 const path = require('path');
 const Student = require('./Models/Student');
 const connect = require('./Config/Connect');
-connect();
 const cors = require('cors');
-const Student = require('./Models/Student');
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 
-app.post("/createStudent", async(req,res)=>{
+// Connect to the database
+connect();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Route to create a student
+app.post("/createStudent", async (req, res) => {
     try {
-        const {name , age  }= req.body;
+        const { name, age } = req.body;
         const newStudent = await Student.create({
             name,
             age,
@@ -21,58 +26,52 @@ app.post("/createStudent", async(req,res)=>{
         });
     } catch (error) {
         return res.status(404).json({
-            Message:"Something went wrong",
+            message: "Something went wrong",
             error: error.message
         });
-        
     }
-
-
-
-
 });
-app,get("/findStudent/:name" , (req,res)=>{
+
+// Route to find a student by name
+app.get("/findStudent/:id", async (req, res) => {
     try {
-        const Student = Student.findOne({name:req.para.name});
-    res.status(200).send(Student);
-        
-    } catch (error) {
-        res.status(404).json({message:"Something Went Wrong",
-            error:error.message
-        });
-        
-    }
-    
-})
-app.post("/updateStudent/:id" , async(req,res)=>{
-    try {
-        const {
-            strengths,weaknesses, interests , preferences 
-        } = req.body;
-        const updatedStudent = await Student.findOneAndUpdate({_id:req.params.id},
-            {
-                strengths:strengths.split(','),
-                preferences:preferences.split(','),
-                interests:interests.split(','),
-                weaknesses:weaknesses.split(',')
-    
-            },
-            {new:true , upsert:true}
-        );
-        res.status(200).send(updatedStudent);
-        
+        const foundStudent = await Student.findOne({ _id: req.params.id });
+        if (!foundStudent) {
+            return res.status(404).send({ message: "Student not found" });
+        }
+        res.status(200).send(foundStudent);
     } catch (error) {
         res.status(404).json({
-            message:"SOmething Went Wrong ",
-            error:error.message,
+            message: "Something went wrong",
+            error: error.message
         });
-        
     }
-    
-
 });
 
+// Route to update a student
+app.post("/updateStudent/:id", async (req, res) => {
+    try {
+        const { strengths, weaknesses, interests, preferences } = req.body;
+        const updatedStudent = await Student.findOneAndUpdate(
+            { _id: req.params.id },
+            {
+                strengths: strengths.split(','),
+                weaknesses: weaknesses.split(','),
+                interests: interests.split(','),
+                preferences: preferences.split(',')
+            },
+            { new: true, upsert: true }
+        );
+        res.status(200).send(updatedStudent);
+    } catch (error) {
+        res.status(404).json({
+            message: "Something went wrong",
+            error: error.message,
+        });
+    }
+});
 
-app.listen(3000, ()=>{
-    console.log("Server Up and Running ");
+// Start the server
+app.listen(3000, () => {
+    console.log("Server is up and running on port 3000");
 });
